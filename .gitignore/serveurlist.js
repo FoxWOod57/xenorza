1e66a202e75f4f3b799d117bf89333d2c9bd78a1
JeanMich <31895237+FoxWOod57@users.noreply.github.com>
2018-07-12 16:55:00 +0200

    Delete serveurlist.js

--- .gitignore/serveurlist.js
+++
@@ -1,9 +0,0 @@
-const Discord = require ("discord.js");
-
-module.exports.run = async(bot, message, args) => {
-  message.channel.send(bot.guilds.map(r => r.name + ` | **${r.memberCount} Membres**`))
-}
-
-module.exports.help = {
-  name: "serveurlist"
-}
